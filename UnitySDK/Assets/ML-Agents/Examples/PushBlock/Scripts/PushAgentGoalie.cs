e0ee8550d164a41fc253d721422067d5c8ef13e4
fr05t3 <kierpersaud@yahoo.ca>
2018-10-14 18:56:40 -0400

    Copy of ML-Agents Beta 0.5.0a, with added scene
    Initial commit, with unity ML-Agents repo pulled down. Added experimental scene PushBlockGoalie, where a new goalie agent was added to attempt to block the original agent from scoring. Note This repo is also missing the Plugins folder, as the files are too large and I'm not buying github large file storage or w.e. Fun stuff :smile:

---
+++ UnitySDK/Assets/ML-Agents/Examples/PushBlock/Scripts/PushAgentGoalie.cs
@@ -0,0 +1,41 @@
+﻿//Put this script on your blue cube.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class PushAgentGoalie : PushAgentBasic
+{
+     public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        goalDetect = block.GetComponent<GoalDetect>();
+        goalDetect.agentGoalie = this;
+    }
+
+
+    /// <summary>
+    /// Called when an agent moves the block into the goal.
+    /// </summary>
+    public override void IScoredAGoal()
+    {
+        // We use a penalty of -5.
+        AddReward(-5f);
+
+        // By marking an agent as done AgentReset() will be called automatically.
+        Done();
+    }
+
+    /// <summary>
+    /// Called every step of the engine. Here the agent takes an action.
+    /// </summary>
+	public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        // Move the agent using the action.
+        MoveAgent(vectorAction);
+
+        // Reward given each step to encourage agent to prolong the game.
+        AddReward(1f / agentParameters.maxStep);
+    }
+}
